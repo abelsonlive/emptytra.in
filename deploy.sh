7fd9e4472a62896619c1fa0249a5e6cf30c37306
Brian Abelson <brianabelson@gmail.com>
2020-03-19 11:07:15 -0400

    switch to cloudfront

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 
 aws --profile brian s3 cp index.html s3://emptytra.in --acl public-read
 aws --profile brian s3 cp about.html s3://emptytra.in --acl public-read
+aws --profile brian cloudfront create-invalidation --distribution-id E22K36V99BLTGI --paths '/*' > /dev/null
