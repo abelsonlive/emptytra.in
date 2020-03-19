be8fd4ddd300d5cd7347822a7c5e87e028ab7b2e
Brian Abelson <brianabelson@gmail.com>
2020-03-19 03:16:06 -0400

    make public

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-aws --profile brian s3 cp index.html s3://emptytra.in
-aws --profile brian s3 cp about.html s3://emptytra.in
+aws --profile brian s3 cp index.html s3://emptytra.in --acl public-read
+aws --profile brian s3 cp about.html s3://emptytra.in --acl public-read
